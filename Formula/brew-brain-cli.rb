dc802a766790f2bf0414bee96ac2eec0871bedfc
Raymon Epping <your@email.com>
2025-07-21 12:54:18 +0200

    🔖 brew-brain-cli: release v1.1.0

--- Formula/brew-brain-cli.rb
+++ Formula/brew-brain-cli.rb
@@ -2,10 +2,10 @@ class BrewBrainCli < Formula
 
   desc "Audit, document, and manage your Homebrew CLI arsenal with one meta-tool"
   homepage "https://github.com/raymonepping/brew_brain_cli"
-  url "https://github.com/raymonepping/homebrew-brew-brain-cli/archive/refs/tags/v1.0.12.tar.gz"
-  sha256 "981a1e6d6873b97dea6f4ab34c8c0dbb2ecf9eedf16ab0357d7cba478fcb026f"
+  url "https://github.com/raymonepping/homebrew-brew-brain-cli/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "952f23049e4e5cb0d05f0c24a8df2c76ec3f61db34495afd915c309cd40d320e"
   license "MIT"
-  version "1.0.12"
+  version "1.1.0"
 
   depends_on "bash"
   depends_on "jq"
